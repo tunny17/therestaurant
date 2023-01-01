cfd2a194922036f273537458a09d25a16fb40284
tunny17 <tvnji17@gmail.com>
2023-01-01 15:13:47 +0100

    base files

--- src/App.js
+++ src/App.js
@@ -1,45 +1,118 @@
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-//   Link
-// } from 'react-router-dom';
-import Home from "./components/Home";
+import React from "react";
+import images from './constants/images';
+import './App.css';
 
 
 function App() {
   return (
-    // <Router>
-    // <div>
-    //   <section>
-    //     <img src="/public/img/chef.svg" alt=""/>
-    //   </section>
-
-    //   <section>
-    //     <nav>
-    //         <ul className='App'>
-    //             <li>
-    //             <Link to="/src/components/Home.js">Home</Link>
-    //             </li>
-    //         </ul>
-    //     </nav>
-    //     </section>
-    //   </div>
-
-    //   <Routes>
-    //     <Route exact path='/' element={< Home />}></Route>
-    //   </Routes>
-
-    // </Router>
-    
     <div>
-      <section>
-        <img src="./img/chef.svg" alt=""/>
+
+      <section className="navigation">
+        <div className="logo-container">
+          <img src={images.Logo} alt="A Logo" />
+        </div>
+
+        {/* main nav */}
+        <div className="nav-container">
+          <nav>
+            <ul>
+              <li>Home</li>
+              <li>Menu</li>
+              <li>Stories</li>
+              <li>About Us</li>
+            </ul>
+          </nav>
+        </div>
+
+        {/* the button and cart side */}
+        <div className="nav-2">
+          <div className="cart-container">
+            <img src={images.Cart} alt="A cart" />
+            <p>2</p>
+          </div>
+          <div className="button-container">
+            <button type="button">LOGIN/SIGNUP</button>
+          </div>
+        </div>
       </section>
-      <br />
-      <section>
-        <Home />
+
+      <section className="hero-section">
+        <div  className="hero-text">
+          <h3>QUICK DELIVERY AT YOUR FINGER TIPS</h3>
+          <img src={images.UnderLine} alt="An underline svg" />
+          <p><span>Order</span> or <span>Reserve</span> your favourite refreshing drink and snacks.</p>
+          <button type="button">EXPLORE MENU</button>
+        </div>
+        <div  className="cup-container">
+          <img src={images.PlasticCup} alt="a plastic cup" className="plastic-cup"/>
+          <img src={images.SplashEffect} alt="splash effect" className="splash-overlay1"/>
+          <img src={images.JuiceSplash} alt="a splash" className="splash-overlay2"/>
+        </div>
       </section>
+
+      <section className="menu-section">
+        <header>
+          <h3>OUR MENUS</h3>
+          <img src={images.UnderLine} alt="an underline" />
+        </header>
+        <div className="drinks">
+          <h3>Drinks</h3>
+          <div className="drinks-container">
+            <div  className="drink">
+              <div  className="drink-details">
+                <h4>ORANGE JUICE</h4>
+                <p>N200</p>
+              </div>
+              <img src={images.Cart} alt="a cart"   className="hidden-cart"/>
+            </div>
+            <div  className="drink drink2">
+              <div  className="drink-details">
+                <h4>ORANGE JUICE</h4>
+                <p>N200</p>
+              </div>
+              <img src={images.Cart} alt="a cart"   className="hidden-cart"/>
+            </div>
+            <div  className="drink drink3">
+              <div  className="drink-details">
+                <h4>ORANGE JUICE</h4>
+                <p>N200</p>
+              </div>
+              <img src={images.Cart} alt="a cart"   className="hidden-cart"/>
+            </div>
+            <div  className="drink drink4">
+              <div  className="drink-details">
+                <h4>ORANGE JUICE</h4>
+                <p>N200</p>
+              </div>
+              <img src={images.Cart} alt="a cart"   className="hidden-cart"/>
+            </div>
+            <div  className="drink drink5">
+              <div  className="drink-details">
+                <h4>ORANGE JUICE</h4>
+                <p>N200</p>
+              </div>
+              <img src={images.Cart} alt="a cart"   className="hidden-cart"/>
+            </div>
+            <div  className="drink drink6">
+              <div  className="drink-details">
+                <h4>ORANGE JUICE</h4>
+                <p>N200</p>
+              </div>
+              <img src={images.Cart} alt="a cart"   className="hidden-cart"/>
+            </div>
+            <div  className="drink drink7">
+              <div  className="drink-details">
+                <h4>ORANGE JUICE</h4>
+                <p>N200</p>
+              </div>
+              <img src={images.Cart} alt="a cart"   className="hidden-cart"/>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      
+
     </div>
   );
 }
---
+++ src/constants/images.js
@@ -0,0 +1,61 @@
+import Logo from '../assets/Logo.svg';
+import Cart from '../assets/Cart.svg'
+import SplashEffect from '../assets/Splash-Effect.svg';
+import JuiceSplash from '../assets/Juice-splash.svg';
+import PlasticCup from '../assets/Plastic-Cup.svg';
+import UnderLine from '../assets/Our-menus-line.svg';
+import Drinks1 from '../assets/drink1.jpg';
+import Drinks2 from '../assets/drink2.jpg';
+import Drinks3 from '../assets/drink3.jpg';
+import Drinks4 from '../assets/drink4.jpg';
+import Drinks5 from '../assets/drink5.jpg';
+import Drinks6 from '../assets/drink6.jpg';
+import Drinks7 from '../assets/drink7.jpg';
+import Snacks1 from '../assets/Snacks-1.jpg';
+import Snacks2 from '../assets/Snacks-2.jpg';
+import Snacks3 from '../assets/Snacks-3.jpg';
+import Snacks4 from '../assets/Snacks-4.jpg';
+import IceCream1 from '../assets/Ice-cream-1.jpg';
+import IceCream2 from '../assets/Ice-cream-2.jpg';
+import IceCream3 from '../assets/Ice-cream-3.jpg';
+import Carousel1 from '../assets/Carousel-1.jpg';
+import Carousel2 from '../assets/Carousel-2.jpg';
+import Carousel3 from '../assets/Carousel-3.jpg';
+import Carousel4 from '../assets/Carousel-4.jpg';
+import Carousel5 from '../assets/Carousel-5.jpg';
+import Carousel6 from '../assets/Carousel-6.jpg';
+import AboutUs from '../assets/About-us-img.jpg';
+
+
+
+const images = {
+    Logo,
+    SplashEffect,
+    JuiceSplash,
+    PlasticCup,
+    Cart,
+    UnderLine,
+    Drinks1,
+    Drinks2,
+    Drinks3,
+    Drinks4,
+    Drinks5,
+    Drinks6,
+    Drinks7,
+    Snacks1,
+    Snacks2,
+    Snacks3,
+    Snacks4,
+    IceCream1,
+    IceCream2,
+    IceCream3,
+    Carousel1,
+    Carousel2,
+    Carousel3,
+    Carousel4,
+    Carousel5,
+    Carousel6,
+    AboutUs
+};
+
+export default images;
